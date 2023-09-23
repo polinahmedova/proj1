a3fd9ea3f2697c52e4502c62b41e19f2cf406067
polinahmedova <polinaahmedova510@gmail.com>
2023-09-23 09:23:52 +0500

    init

---
+++ bin/proj1.dart
@@ -0,0 +1,5 @@
+import 'package:proj1/proj1.dart' as proj1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${proj1.calculate()}!');
+}
---
+++ test/proj1_test.dart
@@ -0,0 +1,8 @@
+import 'package:proj1/proj1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
